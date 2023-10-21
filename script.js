60942c5e189e63a5575da5c3c68874a8c67dd480
Ricardo Martin Hoyos B <94667856+ricardohoycode@users.noreply.github.com>
2023-10-21 02:23:28 -0500

    pequeño cambio en script.js

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ menu.addEventListener('click', () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 })
+const menuIcon = document.getElementById("hambmenuicon");
+const menu = document.querySelector(".porfolio__home--navbar--menu");
+const menuList = document.querySelectorAll(".porfolio__home--navbar--menu li");
+
+menuIcon.addEventListener("click", function () {
+   menu.classList.toggle("menushow");
+   changeMenu();
+});
+
+menuList.forEach((e) => {
+   e.addEventListener("click", function () {
+      menu.classList.remove("menushow");
+      changeMenu();
+   });
+});
+
+function changeMenu() {
+   menu.classList.value.includes("menushow")
+      ? (menuIcon.src = "./src/img/icons/closemenu.svg")
+      : (menuIcon.src = "./src/img/icons/openmenu.svg");
+}
