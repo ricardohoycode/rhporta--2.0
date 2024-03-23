75a5fd80c4262aee83d0a70c32a2067c00a2cd81
Ricardo Martin Hoyos B. <94667856+ricardohoycode@users.noreply.github.com>
2024-03-22 23:18:09 -0500

    SliderSwiper

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
+let navbar = document.querySelector('.navbar')
 
-window.onscroll = () => {
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
+menuBtn.onclick = () =>{
+   menuBtn.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
 }
 
-menu.addEventListener('click', () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-})
+window.onscroll = () =>{
+   menuBtn.classList.remove('fa-times');
+   navbar.classList.remove('active');
+}
 
+var swiper = new Swiper(".teachers-slider", {
+   spaceBetween: 20,
+   grabCursor:true,
+   loop:true,
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+   },
+   breakpoints: {
+      540: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+   },
+});
